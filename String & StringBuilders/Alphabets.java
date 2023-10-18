d73f906aa7d4f2d719927d8944d64b36da5467fa
Nagbhushan Tirth <113358781+nt56@users.noreply.github.com>
2023-10-18 08:01:48 +0530

    Add files via upload

---
+++ String & StringBuilders/Alphabets.java
@@ -0,0 +1,21 @@
+public class Alphabets {
+    public static void main(String[] args) {
+        String series = "";
+        for(int i=0; i<26; i++){
+            char ch = (char)('a' + i);
+            System.out.print(ch +" ");
+        }
+    }
+}
+
+
+//public class Alphabets {
+//    public static void main(String[] args) {
+//        String series = "";
+//        for(int i=0; i<26; i++){
+//            char ch = (char)('a' + i);
+//            series = series + ch;
+//        }
+//        System.out.print(series +" ");
+//    }
+//}
